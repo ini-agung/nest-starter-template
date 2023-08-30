e29749d00b08d776a38f57c00d90e479df268c58
ini-agung <cisea.dev@gmail.com>
2023-08-30 08:43:13 +0700

    update helper

--- libs/helper/src/helper.service.ts
+++ libs/helper/src/helper.service.ts
@@ -6,7 +6,7 @@ import { createCipheriv, createDecipheriv } from 'crypto';
 const algorithm: string = 'aes-256-cbc';
 const key: string = 'MySecretEncryptionKey123!'; // Replace with your own encryption key
 const encryptionKey = generateEncryptionKey(key);
-// pagination.interface.ts
+
 export interface Pagination<T> {
     data: T[];
     total: number;
@@ -17,21 +17,16 @@ export interface Pagination<T> {
 }
 
 export function encryptData(data: string): string {
-
     const iv = crypto.randomBytes(16);
-
     const cipher = createCipheriv(algorithm, encryptionKey, iv);
-
     let encryptedData = cipher.update(JSON.stringify(data), 'utf8', 'hex');
     encryptedData += cipher.final('hex');
-
     return `${iv.toString('hex')}:${encryptedData}`;
 }
 
 export function decryptData(encryptedData: string): string {
     const [ivHex, encryptedDataHex] = encryptedData.split(':');
     const decipher = createDecipheriv(algorithm, encryptionKey, Buffer.from(ivHex, 'hex'));
-
     let decryptedData = decipher.update(encryptedDataHex, 'hex', 'utf8');
     decryptedData += decipher.final('utf8');
     console.log(decryptedData);
@@ -41,7 +36,6 @@ export function decryptData(encryptedData: string): string {
 function generateEncryptionKey(key: string): Buffer {
     const salt = crypto.randomBytes(16);
     const keyLength = 32;
-
     return crypto.pbkdf2Sync(key, salt, 100000, keyLength, 'sha256');
 }
 let currentUserValue: object;
