dea54e88a2456d20b9fc7078d0ae1d5477c156b9
ini-agung <cisea.dev@gmail.com>
2023-08-31 10:28:38 +0700

    update command/documentation

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,34 +1,117 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, Res, Query } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { responseJson } from '@app/response';
 
 @Controller('permissions')
 export class PermissionsController {
-  constructor(private readonly permissionsService: PermissionsService) {}
+  constructor(private readonly permissionsService: PermissionsService) { }
 
+  private _page = parseInt(process.env.PAGINATION_PAGE)
+  private _limit = parseInt(process.env.PAGINATION_LIMIT)
+  /**
+   * Create a new permission.
+   *
+   * @param createPermissionDto - Data to create a new permission.
+   * @param response - HTTP response object.
+   */
   @Post()
-  create(@Body() createPermissionDto: CreatePermissionDto) {
-    return this.permissionsService.create(createPermissionDto);
+  async create(@Body() createPermissionDto: CreatePermissionDto, @Res() response) {
+    const data = {
+      status: true,
+      statusCode: HttpStatus.ACCEPTED,
+      message: 'Success Create New Permission',
+      data: {}
+    };
+    const permission = await this.permissionsService.create(createPermissionDto);
+    data.data = permission;
+    responseJson(data, data.statusCode, response);
   }
 
+  /**
+   * Retrieve all permissions with optional filtering and pagination.
+   *
+   * @param page - Page number for pagination (default: 1).
+   * @param limit - Number of items per page (default: 10).
+   * @param code - Filter by permission's code.
+   * @param response - HTTP response object.
+   */
   @Get()
-  findAll() {
-    return this.permissionsService.findAll();
+  async findAll(
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+    @Query('code') code: string,
+    @Res() response) {
+    const data = {
+      status: true,
+      statusCode: HttpStatus.OK,
+      message: 'Success Get Permissions',
+      data: {}
+    };
+    page = (page == undefined) ? this._page : page;
+    limit = (limit == undefined) ? this._limit : (limit > this._limit) ? this._limit : limit;
+    const permissions = await this.permissionsService.findLike(code, page, limit);
+    data.data = permissions;
+    responseJson(data, data.statusCode, response);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
-  }
 
+  /**
+  * Update an existing permission.
+  *
+  * @param id - ID of the permission to update.
+  * @param updatepermissionDto - Updated permission data.
+  * @param response - HTTP response object.
+  */
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  async update(@Param('id') id: string, @Body() updatepermissionDto: UpdatePermissionDto, @Res() response) {
+    const data = {
+      status: true,
+      statusCode: HttpStatus.ACCEPTED,
+      message: 'Success Update Permission',
+      data: {}
+    };
+    const permission = await this.permissionsService.update(+id, updatepermissionDto);
+    data.data = permission;
+    responseJson(data, data.statusCode, response);
   }
 
+  /**
+   * Soft-delete a permission.
+   *
+   * @param id - ID of the permission to delete.
+   * @param response - HTTP response object.
+   */
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+  async remove(@Param('id') id: string, @Res() response) {
+    const data = {
+      status: true,
+      statusCode: HttpStatus.OK,
+      message: 'Success Delete Permission',
+      data: {}
+    };
+    const permission = await this.permissionsService.remove(+id);
+    data.data = permission;
+    responseJson(data, data.statusCode, response);
+  }
+
+  /**
+   * Restore a previously soft-deleted permission.
+   *
+   * @param id - ID of the permission to restore.
+   * @param response - HTTP response object.
+   */
+  @Patch(':id/restore')
+  async restore(@Param('id') id: number, @Res() response) {
+    const restoredUser = await this.permissionsService.restore(id);
+    const data = {
+      status: true,
+      statusCode: HttpStatus.OK,
+      message: 'Success Restore Permission',
+      data: {}
+    };
+    data.data = restoredUser;
+    responseJson(data, data.statusCode, response);
   }
 }
--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,26 +1,198 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Permission } from './entities/permission.entity';
+import { Repository } from 'typeorm';
+import { Pagination } from '@app/helper';
 
 @Injectable()
 export class PermissionsService {
-  create(createPermissionDto: CreatePermissionDto) {
-    return 'This action adds a new permission';
+  constructor(
+    @InjectRepository(Permission)
+    private permissionsRepository: Repository<Permission>,
+  ) { }
+  private readonly logger = new Logger(PermissionsService.name);
+
+  /**
+     * Create a new permission.
+     *
+     * @param createPermissionDto - Data to create a new permission.
+     * @returns The created permission.
+     * @throws ConflictException if there's an error while creating the permission.
+     */
+  async create(createPermissionDto: CreatePermissionDto) {
+    try {
+      const permission = await this.permissionsRepository.create(createPermissionDto);
+      return await this.permissionsRepository.save(permission);
+    } catch (error) {
+      this.logger.error(`Error saving ${error.message}`);
+      const data = {
+        status: false,
+        statusCode: HttpStatus.CONFLICT,
+        message: error.sqlMessage,
+        data: {}
+      };
+      data.data = error.message;
+      throw new ConflictException(data, { cause: new Error() });
+    }
   }
 
-  findAll() {
-    return `This action returns all permissions`;
+
+  /**
+   * Retrieve permissions based on query parameters.
+   *
+   * @param code - Filter by permission's full code.
+   * @param page - Page number for pagination (default: 1).
+   * @param limit - Number of items per page (default: 10).
+   * @returns An array of permissions that match the query criteria.
+   * @throws ConflictException if there's an error while retrieving permissions.
+   */
+  async findLike(
+    code: string,
+    page: number,
+    limit: number,
+  ): Promise<Pagination<any>> {
+    try {
+      const queryBuilder = await this.permissionsRepository
+        .createQueryBuilder('permission')
+        .select([
+          'permission.id',
+          'permission.code',
+          'permission.description',
+        ])
+        .where('permission.deletedAt IS NULL')
+
+      if (code) {
+        queryBuilder.andWhere('((permission.code LIKE :code) OR (permission.description LIKE :code))', { code: `%${code}%` })
+      }
+      // this.logger.log(queryBuilder);
+      const permissionCounts = await queryBuilder.orderBy('permission.id', 'ASC').getMany();
+      const total = permissionCounts.length;
+      const startIdx = (page - 1) * limit;
+      const endIdx = parseInt(startIdx.toString()) + parseInt(limit.toString());
+      const data = permissionCounts.slice(startIdx, endIdx);
+      return {
+        data,
+        total,
+        currentPage: page,
+        perPage: limit,
+        prevPage: page > 1 ? `/students?page=${(parseInt(page.toString()) - 1)}` : undefined,
+        nextPage: endIdx < total ? `/students?page=${(parseInt(page.toString()) + 1)}` : undefined,
+      };
+    } catch (error) {
+      this.logger.error(`Error find permission :  ${error.message}`);
+      const data = {
+        status: false,
+        statusCode: HttpStatus.CONFLICT,
+        message: error.sqlMessage,
+        data: {}
+      };
+      data.data = error.message;
+      throw new ConflictException(data, { cause: new Error() });
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} permission`;
+  /**
+   * Update an existing permission.
+   *
+   * @param id - ID of the permission to update.
+   * @param updatePermissionDto - Updated permission data.
+   * @returns The updated permission.
+   * @throws ConflictException if the permission doesn't exist or there's an error during update.
+   */
+  async update(id: number, updatePermissionDto: UpdatePermissionDto) {
+    try {
+      const permission = await this.permissionsRepository.findOneBy({ id });
+      if (!permission) {
+        const data = {
+          status: false,
+          statusCode: HttpStatus.NOT_FOUND,
+          message: `permission with id ${id} is doesnt exists`,
+          data: {}
+        };
+        throw new ConflictException(data, { cause: new Error() });
+      }
+      Object.assign(permission, updatePermissionDto);
+      return this.permissionsRepository.save(permission);
+    } catch (error) {
+      this.logger.error(`Error find permission :  ${error.message}`);
+      const data = {
+        status: false,
+        statusCode: HttpStatus.CONFLICT,
+        message: error.sqlMessage,
+        data: {}
+      };
+      data.data = error.message;
+      throw new ConflictException(data, { cause: new Error() });
+    }
   }
 
-  update(id: number, updatePermissionDto: UpdatePermissionDto) {
-    return `This action updates a #${id} permission`;
+  /**
+   * Soft-delete a permission.
+   *
+   * @param id - ID of the permission to delete.
+   * @returns The soft-deleted permission.
+   * @throws ConflictException if the permission doesn't exist or there's an error during deletion.
+   */
+  async remove(id: number) {
+    try {
+      const permission = await this.permissionsRepository.findOneBy({ id });
+      if (!permission) {
+        const data = {
+          status: false,
+          statusCode: HttpStatus.NOT_FOUND,
+          message: `permission with id ${id} is doesnt exists`,
+          data: {}
+        };
+        throw new ConflictException(data, { cause: new Error() });
+      }
+      // Instead of a hard delete, mark the permission as deleted (soft delete)
+      permission.deletedAt = new Date();; // Assuming there's a property called "deleted" on the permission entity
+      return await this.permissionsRepository.save(permission); // Save to persist the soft delete
+    } catch (error) {
+      this.logger.error(`Error find permission :   ${error.message}`);
+      const data = {
+        status: false,
+        statusCode: HttpStatus.CONFLICT,
+        message: error.sqlMessage,
+        data: {}
+      };
+      data.data = error.message;
+      throw new ConflictException(data, { cause: new Error() });
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} permission`;
+  /**
+   * Restore a previously soft-deleted permission.
+   *
+   * @param id - ID of the permission to restore.
+   * @returns The restored permission or null if not found.
+   * @throws ConflictException if there's an error during restoration.
+   */
+  async restore(id: number): Promise<Permission> {
+    try {
+      const userToRestore = await this.permissionsRepository
+        .createQueryBuilder('user')
+        .withDeleted() // Include soft-deleted entities
+        .where('user.id = :id', { id })
+        .getOne();
+
+      if (userToRestore) {
+        userToRestore.deletedAt = null; // Set deletedAt back to null
+        return this.permissionsRepository.save(userToRestore);
+      }
+      return null; // User not found
+    } catch (error) {
+      this.logger.error(`Error find permission :   ${error.message}`);
+      const data = {
+        status: false,
+        statusCode: HttpStatus.CONFLICT,
+        message: error.sqlMessage,
+        data: {}
+      };
+      data.data = error.message;
+      throw new ConflictException(data, { cause: new Error() });
+    }
   }
 }
